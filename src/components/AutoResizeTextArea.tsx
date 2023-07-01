3f6ef5adcf593c5fb55af9fcac5e9328a93807d0
arcticfly <41524992+arcticfly@users.noreply.github.com>
2023-07-01 08:52:20 -0700

    Add AutoResizeTextArea (#7)

---
+++ src/components/AutoResizeTextArea.tsx
@@ -0,0 +1,24 @@
+import { Textarea, type TextareaProps } from "@chakra-ui/react";
+import ResizeTextarea from "react-textarea-autosize";
+import React from "react";
+
+export const AutoResizeTextarea: React.ForwardRefRenderFunction<
+  HTMLTextAreaElement,
+  TextareaProps
+> = (props, ref) => {
+  return (
+    <Textarea
+      minH="unset"
+      overflow="hidden"
+      w="100%"
+      resize="none"
+      ref={ref}
+      minRows={1}
+      transition="height none"
+      as={ResizeTextarea}
+      {...props}
+    />
+  );
+};
+
+export default React.forwardRef(AutoResizeTextarea);
