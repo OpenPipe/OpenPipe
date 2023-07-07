6aadeeba96b6e94d711053c02dc101de9b41baba
David Corbitt <davidlcorbitt@gmail.com>
2023-07-07 14:43:20 -0700

    Use humanize in formatTimePast

--- src/utils/dayjs.ts
+++ src/utils/dayjs.ts
@@ -1,16 +1,20 @@
 import dayjs from "dayjs";
-import pluralize from "pluralize";
+import duration from "dayjs/plugin/duration";
+import relativeTime from "dayjs/plugin/relativeTime";
+
+dayjs.extend(duration);
+dayjs.extend(relativeTime);
 
 export const formatTimePast = (date: Date) => {
     const now = dayjs();
     const dayDiff = Math.floor(now.diff(date, "day"));
-    if (dayDiff > 0) return `${pluralize('day', dayDiff, true)} ago`;
+    if (dayDiff > 0) return dayjs.duration(-dayDiff, "days").humanize(true);
     
     const hourDiff = Math.floor(now.diff(date, "hour"));
-    if (hourDiff > 0) return `${pluralize('hour', hourDiff, true)} ago`;
+    if (hourDiff > 0) return dayjs.duration(-hourDiff, "hours").humanize(true);
 
     const minuteDiff = Math.floor(now.diff(date, "minute"));
-    if (minuteDiff > 0) return `${pluralize('minute', minuteDiff, true)} ago`;
+    if (minuteDiff > 0) return dayjs.duration(-minuteDiff, "minutes").humanize(true);
 
     return 'a few seconds ago'
 };
