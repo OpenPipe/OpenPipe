f269508e9e75651cae391609e7ea7c54af960ab8
Jaren <50216013+jarenm@users.noreply.github.com>
2023-07-07 20:33:43 -0600

    add padding and style

--- src/components/tooltip/CostTooltip.tsx
+++ src/components/tooltip/CostTooltip.tsx
@@ -1,4 +1,4 @@
-import { HStack, Icon, Text, Tooltip, type TooltipProps, VStack } from "@chakra-ui/react";
+import { HStack, Icon, Text, Tooltip, type TooltipProps, VStack, Divider } from "@chakra-ui/react";
 import { BsCurrencyDollar } from "react-icons/bs";
 
 type CostTooltipProps = {
@@ -17,25 +17,33 @@ export const CostTooltip = ({
   return (
     <Tooltip
       borderRadius="8"
-      color="black"
+      color="gray.800"
       bgColor="gray.50"
+      py={2}
       hasArrow
       label={
-        <VStack>
-          <HStack spacing={0}>
-            <Icon as={BsCurrencyDollar} />
-            <Text>{cost.toFixed(6)}</Text>
-          </HStack>
-          <HStack>
-            <VStack>
-              <Text>Prompt Tokens</Text>
-              <Text>{promptTokens ?? 0}</Text>
-            </VStack>
-            <VStack>
-              <Text>Completion Tokens</Text>
-              <Text>{completionTokens ?? 0}</Text>
-            </VStack>
-          </HStack>
+        <VStack fontSize="sm" w="200">
+          <VStack>
+            <Text fontWeight="bold">Cost</Text>
+            <HStack spacing={0}>
+              <Icon as={BsCurrencyDollar} />
+              <Text>{cost.toFixed(6)}</Text>
+            </HStack>
+          </VStack>
+          <VStack>
+            <Text fontWeight="bold">Tokens</Text>
+            <HStack>
+              <VStack w="28">
+                <Text>Prompt</Text>
+                <Text>{promptTokens ?? 0}</Text>
+              </VStack>
+              <Divider borderColor="gray.200" h={12} orientation="vertical" />
+              <VStack w="28">
+                <Text whiteSpace="nowrap">Completion</Text>
+                <Text>{completionTokens ?? 0}</Text>
+              </VStack>
+            </HStack>
+          </VStack>
         </VStack>
       }
       {...props}
