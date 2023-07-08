bdee7480f3a671922b88a1625626db297bca3e1b
David Corbitt <davidlcorbitt@gmail.com>
2023-07-08 10:22:54 -0700

    Show tooltip on mobile, modify styles

--- src/components/tooltip/CostTooltip.tsx
+++ src/components/tooltip/CostTooltip.tsx
@@ -19,25 +19,27 @@ export const CostTooltip = ({
       borderRadius="8"
       color="gray.800"
       bgColor="gray.50"
+      borderWidth={1}
       py={2}
       hasArrow
+      shouldWrapChildren
       label={
-        <VStack fontSize="sm" w="200">
-          <VStack>
+        <VStack fontSize="sm" w="200" spacing={4}>
+          <VStack spacing={0}>
             <Text fontWeight="bold">Cost</Text>
             <HStack spacing={0}>
               <Icon as={BsCurrencyDollar} />
               <Text>{cost.toFixed(6)}</Text>
             </HStack>
           </VStack>
-          <VStack>
+          <VStack spacing={0}>
             <Text fontWeight="bold">Tokens</Text>
             <HStack>
               <VStack w="28">
                 <Text>Prompt</Text>
                 <Text>{promptTokens ?? 0}</Text>
               </VStack>
-              <Divider borderColor="gray.200" h={12} orientation="vertical" />
+              <Divider borderColor="gray.200" h={8} orientation="vertical" />
               <VStack w="28">
                 <Text whiteSpace="nowrap">Completion</Text>
                 <Text>{completionTokens ?? 0}</Text>
