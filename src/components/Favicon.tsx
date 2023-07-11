31c581d0fbb3e94525ae640b413a691e6356fa30
David Corbitt <davidlcorbitt@gmail.com>
2023-07-11 13:53:55 -0600

    Update logo

--- src/components/Favicon.tsx
+++ src/components/Favicon.tsx
@@ -7,7 +7,6 @@ export default function Favicon() {
       <link rel="icon" type="image/png" sizes="32x32" href="/favicons/favicon-32x32.png" />
       <link rel="icon" type="image/png" sizes="16x16" href="/favicons/favicon-16x16.png" />
       <link rel="manifest" href="/favicons/site.webmanifest" />
-      <link rel="mask-icon" href="/favicons/safari-pinned-tab.svg" color="#5bbad5" />
       <link rel="shortcut icon" href="/favicons/favicon.ico" />
       <meta name="msapplication-TileColor" content="#da532c" />
       <meta name="msapplication-config" content="/favicons/browserconfig.xml" />
