df121913e1db043c09c80f76f96b0428db12951e
David Corbitt <davidlcorbitt@gmail.com>
2023-07-14 12:44:28 -0600

    Add newlines to ends of files

--- src/server/utils/queueLLMRetrievalTask.ts
+++ src/server/utils/queueLLMRetrievalTask.ts
@@ -1,23 +1,22 @@
 import { prisma } from "../db";
 import { queryLLM } from "../tasks/queryLLM.task";
 
-
 export const queueLLMRetrievalTask = async (cellId: string) => {
-    const updatedCell = await prisma.scenarioVariantCell.update({
-        where: {
-            id: cellId,
-        },
-        data: {
-            retrievalStatus: "PENDING",
-            errorMessage: null,
-        },
-        include: {
-            modelOutput: true,
-        }
-    });
-            
-    // @ts-expect-error we aren't passing the helpers but that's ok
-    void queryLLM.task.handler({ scenarioVariantCellId: cellId }, { logger: console });
+  const updatedCell = await prisma.scenarioVariantCell.update({
+    where: {
+      id: cellId,
+    },
+    data: {
+      retrievalStatus: "PENDING",
+      errorMessage: null,
+    },
+    include: {
+      modelOutput: true,
+    },
+  });
 
-    return updatedCell;
-}
+  // @ts-expect-error we aren't passing the helpers but that's ok
+  void queryLLM.task.handler({ scenarioVariantCellId: cellId }, { logger: console });
+
+  return updatedCell;
+};
