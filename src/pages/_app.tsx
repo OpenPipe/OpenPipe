672716edbf226972ee025e192c96cab3228831f5
David Corbitt <davidlcorbitt@gmail.com>
2023-07-31 16:35:30 -0700

    Update meta tags

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -21,8 +21,8 @@ const MyApp: AppType<{ session: Session | null }> = ({
           name="viewport"
           content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0"
         />
-        <meta name="og:title" content="OpenPipe" />
-        <meta name="og:description" content="Tinker with the latest LLMs." />
+        <meta name="og:title" content="OpenPipe: Open-Source Lab for LLMs" />
+        <meta name="og:description" content="OpenPipe is a powerful playground for quickly optimizing performance, cost, and speed across models." />
         <meta name="og:image" content="/og.png" />
         <meta property="og:image:height" content="630" />
         <meta property="og:image:width" content="1200" />
