6316eaae6d423916fad1a02353ccc04272d13079
Kyle Corbitt <kyle@corbt.com>
2023-07-31 18:03:51 -0700

    dummy key at build time

--- src/server/utils/openai.ts
+++ src/server/utils/openai.ts
@@ -2,4 +2,5 @@ import { env } from "~/env.mjs";
 
 import OpenAI from "openai";
 
-export const openai = new OpenAI({ apiKey: env.OPENAI_API_KEY ?? "" });
+// Set a dummy key so it doesn't fail at build time
+export const openai = new OpenAI({ apiKey: env.OPENAI_API_KEY ?? "dummy-key" });
