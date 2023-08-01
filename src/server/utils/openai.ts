8513924ea5ee91c39f1b016aba0556065a495f72
Kyle Corbitt <kyle@corbt.com>
2023-07-31 17:39:45 -0700

    give the openai lib a dummy default value to try to fix the build

--- src/server/utils/openai.ts
+++ src/server/utils/openai.ts
@@ -2,4 +2,4 @@ import { env } from "~/env.mjs";
 
 import OpenAI from "openai";
 
-export const openai = new OpenAI({ apiKey: env.OPENAI_API_KEY });
+export const openai = new OpenAI({ apiKey: env.OPENAI_API_KEY ?? "" });
