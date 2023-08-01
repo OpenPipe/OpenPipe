156f248c3ac553c3f40b3cfd145753a8f5291097
Kyle Corbitt <kyle@corbt.com>
2023-08-01 16:43:35 -0700

    add created_at and updated_at to users

---
+++ src/server/scripts/studio-prod.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+set -e
+cd "$(dirname "$0")/../../.."
+
+
+set -o allexport
+source .env
+set +o allexport
+
+echo "Connecting to prod db"
+DATABASE_URL=$PROD_DATABASE_URL pnpm prisma studio
