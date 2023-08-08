ea91d692d3cd3436032932dd12918b530674174e
David Corbitt <davidlcorbitt@gmail.com>
2023-08-08 11:45:03 -0700

    Use crypto-random-string

--- app/src/server/utils/generateApiKey.ts
+++ app/src/server/utils/generateApiKey.ts
@@ -1,11 +1,5 @@
+import cryptoRandomString from "crypto-random-string";
+
 const KEY_LENGTH = 42;
 
-export const generateApiKey = () => {
-  const chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-  let randomChars = "";
-  for (let i = 0; i < KEY_LENGTH; i++) {
-    randomChars += chars.charAt(Math.floor(Math.random() * chars.length));
-  }
-
-  return `opc_${randomChars}`;
-};
+export const generateApiKey = () => `opc_${cryptoRandomString({ length: KEY_LENGTH })}`;
