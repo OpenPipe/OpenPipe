1b2b6c145655d607c873e4aa2cd3a8f6d1c8d9b7
David Corbitt <davidlcorbitt@gmail.com>
2023-08-08 18:19:59 -0700

    Send organizationId in fork mutation

--- app/src/components/experiments/ExperimentHeaderButtons/useOnForkButtonPressed.tsx
+++ app/src/components/experiments/ExperimentHeaderButtons/useOnForkButtonPressed.tsx
@@ -3,18 +3,23 @@ import { api } from "~/utils/api";
 import { useExperiment, useHandledAsyncCallback } from "~/utils/hooks";
 import { signIn, useSession } from "next-auth/react";
 import { useRouter } from "next/router";
+import { useAppStore } from "~/state/store";
 
 export const useOnForkButtonPressed = () => {
   const router = useRouter();
 
   const user = useSession().data;
   const experiment = useExperiment();
+  const selectedOrgId = useAppStore((state) => state.selectedOrgId);
 
   const forkMutation = api.experiments.fork.useMutation();
 
   const [onFork, isForking] = useHandledAsyncCallback(async () => {
-    if (!experiment.data?.id) return;
-    const forkedExperimentId = await forkMutation.mutateAsync({ id: experiment.data.id });
+    if (!experiment.data?.id || !selectedOrgId) return;
+    const forkedExperimentId = await forkMutation.mutateAsync({
+      id: experiment.data.id,
+      organizationId: selectedOrgId,
+    });
     await router.push({ pathname: "/experiments/[id]", query: { id: forkedExperimentId } });
   }, [forkMutation, experiment.data?.id, router]);
 
