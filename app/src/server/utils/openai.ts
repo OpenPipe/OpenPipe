53b3c4db7ca5a1e8916a28b704be84219231c493
David Corbitt <davidlcorbitt@gmail.com>
2023-08-11 22:55:42 -0700

    Allow custom config file

--- app/src/server/utils/openai.ts
+++ app/src/server/utils/openai.ts
@@ -1,13 +1,23 @@
+import { type ClientOptions, default as OriginalOpenAI } from "openai";
+import fs from "fs";
+import path from "path";
+
 import { env } from "~/env.mjs";
 
-import { default as OriginalOpenAI } from "openai";
-// import { OpenAI } from "openpipe";
+import { OpenAI } from "~/../../client-libs/typescript/dist";
 
-const openAIConfig = { apiKey: env.OPENAI_API_KEY ?? "dummy-key" };
+let config: ClientOptions;
 
-// Set a dummy key so it doesn't fail at build time
-// export const openai = env.OPENPIPE_API_KEY
-//   ? new OpenAI.OpenAI(openAIConfig)
-//   : new OriginalOpenAI(openAIConfig);
+try {
+  // Allow developers to override the config with a local file
+  const jsonData = fs.readFileSync(
+    path.join(path.dirname(import.meta.url).replace("file://", ""), "./openaiCustomConfig.json"),
+    "utf8",
+  );
+  config = JSON.parse(jsonData.toString());
+} catch (error) {
+  // Set a dummy key so it doesn't fail at build time
+  config = { apiKey: env.OPENAI_API_KEY ?? "dummy-key" };
+}
 
-export const openai = new OriginalOpenAI(openAIConfig);
+export const openai = env.OPENPIPE_API_KEY ? new OpenAI.OpenAI(config) : new OriginalOpenAI(config);
