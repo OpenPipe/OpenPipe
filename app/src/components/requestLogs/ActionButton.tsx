7e18d8205d153a260687a9f308bc761a2555f30e
David Corbitt <davidlcorbitt@gmail.com>
2023-08-13 03:20:18 -0700

    Handle isLoading in ActionButton

--- app/src/components/requestLogs/ActionButton.tsx
+++ app/src/components/requestLogs/ActionButton.tsx
@@ -1,9 +1,10 @@
-import { Button, HStack, type ButtonProps, Icon, Text } from "@chakra-ui/react";
+import { Button, HStack, type ButtonProps, Icon, Text, Spinner } from "@chakra-ui/react";
 import { type IconType } from "react-icons";
 
 const ActionButton = ({
   icon,
   label,
+  isLoading,
   ...buttonProps
 }: { icon: IconType; label: string } & ButtonProps) => {
   return (
@@ -20,7 +21,7 @@ const ActionButton = ({
       {...buttonProps}
     >
       <HStack spacing={1}>
-        {icon && <Icon as={icon} />}
+        <Icon as={isLoading ? Spinner : icon} />
         <Text>{label}</Text>
       </HStack>
     </Button>
