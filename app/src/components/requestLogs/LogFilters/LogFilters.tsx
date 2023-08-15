05e774d02181d0bab6abd5236c104f6d1327553c
David Corbitt <davidlcorbitt@gmail.com>
2023-08-14 20:47:18 -0700

    Style filters title

--- app/src/components/requestLogs/LogFilters/LogFilters.tsx
+++ app/src/components/requestLogs/LogFilters/LogFilters.tsx
@@ -7,8 +7,18 @@ import LogFilter from "./LogFilter";
 const LogFilters = () => {
   const filters = useAppStore((s) => s.logFilters.filters);
   return (
-    <VStack bgColor="white" borderRadius={8} borderWidth={1} w="full" alignItems="flex-start" p={4}>
-      <Text>Filters</Text>
+    <VStack
+      bgColor="white"
+      borderRadius={8}
+      borderWidth={1}
+      w="full"
+      alignItems="flex-start"
+      p={4}
+      spacing={4}
+    >
+      <Text fontWeight="bold" color="gray.500">
+        Filters
+      </Text>
       {filters.map((filter, index) => (
         <LogFilter key={index} filter={filter} index={index} />
       ))}
