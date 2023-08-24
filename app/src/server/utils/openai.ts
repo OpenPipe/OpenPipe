19936667ed9fa531f97a142e92eec50519120d7f
David Corbitt <davidlcorbitt@gmail.com>
2023-08-23 23:25:20 -0700

    Remove openpipe object from config

--- app/src/server/utils/openai.ts
+++ app/src/server/utils/openai.ts
@@ -17,10 +17,6 @@ try {
   // Set a dummy key so it doesn't fail at build time
   config = {
     apiKey: env.OPENAI_API_KEY ?? "dummy-key",
-    openpipe: {
-      apiKey: env.OPENPIPE_API_KEY,
-      baseUrl: "http://localhost:3000/api/v1",
-    },
   };
 }
 
