002276f0d356295e12d2f77e8b1bd5ded6363da6
David Corbitt <davidlcorbitt@gmail.com>
2023-10-03 14:21:11 -0700

    Remove test for marking fine-tune as deployed

--- trainer/trainer/test_client.py
+++ trainer/trainer/test_client.py
@@ -1,15 +1,8 @@
 from dotenv import load_dotenv
 import pytest
 import os
-import json
-from trainer.api_client.models.record_status_update_json_body import (
-    RecordStatusUpdateJsonBody,
-)
-from trainer.api_client.models.record_status_update_json_body_status import (
-    RecordStatusUpdateJsonBodyStatus,
-)
 from trainer.shared import configured_client
-from trainer.api_client.api.default import get_training_info, record_status_update
+from trainer.api_client.api.default import get_training_info
 
 load_dotenv()
 
@@ -20,15 +13,3 @@ def test_get_training_info():
         fine_tune_id=os.environ["FINE_TUNE_ID"],
     )
     print("training_info is", training_info)
-
-
-@pytest.mark.focus
-def test_mark_deployed():
-    resp = record_status_update.sync(
-        client=configured_client,
-        json_body=RecordStatusUpdateJsonBody(
-            fine_tune_id=os.environ["FINE_TUNE_ID"],
-            status=RecordStatusUpdateJsonBodyStatus.DEPLOYED,
-        ),
-    )
-    print("resp is", resp)
