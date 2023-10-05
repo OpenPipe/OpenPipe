a71bd331e2405e7af56e054c8dd7571aacd5c995
David Corbitt <davidlcorbitt@gmail.com>
2023-10-04 17:56:34 -0700

    Check db connection in healthcheck

--- app/src/server/api/routers/healthcheck.router.ts
+++ app/src/server/api/routers/healthcheck.router.ts
@@ -1,7 +1,11 @@
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
+import { prisma } from "~/server/db";
 
 export const healthcheckRouter = createTRPCRouter({
-  healthcheck: publicProcedure.query(() => {
+  healthcheck: publicProcedure.query(async () => {
+    // check database connection
+    await prisma.experiment.findFirst();
+
     return { status: "ok" };
   }),
 });
