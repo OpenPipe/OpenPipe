6dd1eb984e5b312d4ebba3282d55c5c455fbfa4c
Kyle Corbitt <kyle@corbt.com>
2023-10-08 10:27:44 -0700

    Use Sentry for APM
    I tried a bunch of different ways to get the Sentry integration working properly but this seems like the one that give us the most info. For each query we can see the time spent in specific http and database queries as well as the request time overall.

--- app/sentry.server.config.ts
+++ app/sentry.server.config.ts
@@ -2,9 +2,13 @@
 // The config you add here will be used whenever the server handles a request.
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/
 
-import * as Sentry from "@sentry/nextjs";
+import * as SentryModule from "@sentry/nextjs";
 import { isError } from "lodash-es";
 import { env } from "~/env.mjs";
+import { prisma } from "~/server/db";
+import { ensureDefaultExport } from "~/utils/utils";
+
+const Sentry = ensureDefaultExport(SentryModule);
 
 if (env.NEXT_PUBLIC_SENTRY_DSN) {
   Sentry.init({
@@ -12,6 +16,11 @@ if (env.NEXT_PUBLIC_SENTRY_DSN) {
 
     // Adjust this value in production, or use tracesSampler for greater control
     tracesSampleRate: 1,
+
+    integrations: [
+      ...Sentry.autoDiscoverNodePerformanceMonitoringIntegrations(),
+      new Sentry.Integrations.Prisma({ client: prisma }),
+    ],
 
     // Setting this option to true will print useful information to the console while you're setting up Sentry.
     debug: false,
