9ddfae54b31c2d45c152aaa2b9784e20b5155428
David Corbitt <davidlcorbitt@gmail.com>
2023-11-15 01:07:12 -0800

    Show indicator when dataset entry is loading

--- app/src/components/datasets/DatasetContentTabs/General/DatasetEntriesTable/DatasetEntryEditorDrawer.tsx
+++ app/src/components/datasets/DatasetContentTabs/General/DatasetEntriesTable/DatasetEntryEditorDrawer.tsx
@@ -15,6 +15,7 @@ import {
   Divider,
   Icon,
   Collapse,
+  Spinner,
 } from "@chakra-ui/react";
 import { type ChatCompletionMessageParam } from "openai/resources/chat";
 import { BsPlus } from "react-icons/bs";
@@ -53,6 +54,13 @@ function DatasetEntryEditorDrawer({
     null,
   );
   const [historyVisible, setHistoryVisible] = useState(false);
+
+  const drawerIsOpen = !!datasetEntryId;
+  const [entryInitiallyLoaded, setEntryInitiallyLoaded] = useState(false);
+  useEffect(() => {
+    if (!drawerIsOpen) setEntryInitiallyLoaded(false);
+    if (datasetEntry?.persistentId) setEntryInitiallyLoaded(true);
+  }, [drawerIsOpen, datasetEntry?.persistentId]);
 
   useEffect(() => {
     if (savedInputMessages && savedOutputMessage) {
@@ -129,110 +137,116 @@ function DatasetEntryEditorDrawer({
           </VStack>
         </DrawerHeader>
         <DrawerBody h="full" pb={4} bgColor="orange.50" id={CONTAINER_ID}>
-          <VStack w="full" spacing={12} pb={4} id={CONTENT_ID}>
-            {datasetEntry && (
-              <VStack
-                w="full"
-                alignItems="flex-start"
-                fontSize="xs"
-                bgColor="gray.50"
-                borderRadius={8}
-                borderWidth={1}
-                borderColor="gray.200"
-                p={4}
-              >
-                {datasetEntry?.importId && (
-                  <>
-                    <Text>
-                      <Text as="span" fontWeight="bold">
-                        Import ID:
-                      </Text>{" "}
-                      {datasetEntry.importId}
-                    </Text>
-                  </>
-                )}
-                <Text>
-                  <Text as="span" fontWeight="bold">
-                    Last Updated:
-                  </Text>{" "}
-                  {dayjs(datasetEntry.createdAt).format("MMMM D h:mm A")}
-                </Text>
-                <VStack w="full" alignItems="flex-start" spacing={0} pt={4}>
-                  <HStack
-                    _hover={{ textDecoration: "underline" }}
-                    onClick={() => setHistoryVisible(!historyVisible)}
-                    cursor="pointer"
-                    fontWeight="bold"
-                    spacing={0.5}
-                  >
-                    <Text>Version History</Text>
-                    <Icon
-                      as={historyVisible ? FiChevronUp : FiChevronDown}
-                      strokeWidth={3}
-                      boxSize={3.5}
-                      pt={0.5}
-                    />
-                  </HStack>
-                  <Collapse in={historyVisible} unmountOnExit={true}>
-                    <VStack align="stretch" spacing={0} pt={2}>
-                      {datasetEntry?.history.map((entry, i) => (
-                        <DatasetEntryHistoryRow key={entry.id} entry={entry} isFirst={i === 0} />
-                      ))}
-                    </VStack>
-                  </Collapse>
+          {!entryInitiallyLoaded ? (
+            <VStack w="full" h="full" justifyContent="center">
+              <Spinner boxSize={10} />
+            </VStack>
+          ) : (
+            <VStack w="full" spacing={12} pb={4} id={CONTENT_ID}>
+              {datasetEntry && (
+                <VStack
+                  w="full"
+                  alignItems="flex-start"
+                  fontSize="xs"
+                  bgColor="gray.50"
+                  borderRadius={8}
+                  borderWidth={1}
+                  borderColor="gray.200"
+                  p={4}
+                >
+                  {datasetEntry?.importId && (
+                    <>
+                      <Text>
+                        <Text as="span" fontWeight="bold">
+                          Import ID:
+                        </Text>{" "}
+                        {datasetEntry.importId}
+                      </Text>
+                    </>
+                  )}
+                  <Text>
+                    <Text as="span" fontWeight="bold">
+                      Last Updated:
+                    </Text>{" "}
+                    {dayjs(datasetEntry.createdAt).format("MMMM D h:mm A")}
+                  </Text>
+                  <VStack w="full" alignItems="flex-start" spacing={0} pt={4}>
+                    <HStack
+                      _hover={{ textDecoration: "underline" }}
+                      onClick={() => setHistoryVisible(!historyVisible)}
+                      cursor="pointer"
+                      fontWeight="bold"
+                      spacing={0.5}
+                    >
+                      <Text>Version History</Text>
+                      <Icon
+                        as={historyVisible ? FiChevronUp : FiChevronDown}
+                        strokeWidth={3}
+                        boxSize={3.5}
+                        pt={0.5}
+                      />
+                    </HStack>
+                    <Collapse in={historyVisible} unmountOnExit={true}>
+                      <VStack align="stretch" spacing={0} pt={2}>
+                        {datasetEntry?.history.map((entry, i) => (
+                          <DatasetEntryHistoryRow key={entry.id} entry={entry} isFirst={i === 0} />
+                        ))}
+                      </VStack>
+                    </Collapse>
+                  </VStack>
                 </VStack>
+              )}
+              <VStack w="full" alignItems="flex-start">
+                <Text fontWeight="bold">Input</Text>
+                {inputMessagesToSave.map((message, i) => {
+                  return (
+                    <>
+                      <Divider key={`divider-${i}`} my={4} />
+                      <EditableMessage
+                        key={i}
+                        message={message}
+                        onEdit={(message) => {
+                          const newInputMessages = [...inputMessagesToSave];
+                          newInputMessages[i] = message;
+                          setInputMessagesToSave(newInputMessages);
+                        }}
+                        onDelete={() => {
+                          const newInputMessages = [...inputMessagesToSave];
+                          newInputMessages.splice(i, 1);
+                          setInputMessagesToSave(newInputMessages);
+                        }}
+                        ruleMatches={datasetEntry?.matchedRules}
+                      />
+                    </>
+                  );
+                })}
+                <Divider my={4} />
+                <Button
+                  w="full"
+                  onClick={() =>
+                    setInputMessagesToSave([...inputMessagesToSave, { role: "user", content: "" }])
+                  }
+                  variant="outline"
+                  color="gray.500"
+                  _hover={{ bgColor: "orange.100" }}
+                >
+                  <HStack spacing={0}>
+                    <Text>Add Message</Text>
+                    <Icon as={BsPlus} boxSize={6} />
+                  </HStack>
+                </Button>
               </VStack>
-            )}
-            <VStack w="full" alignItems="flex-start">
-              <Text fontWeight="bold">Input</Text>
-              {inputMessagesToSave.map((message, i) => {
-                return (
-                  <>
-                    <Divider key={`divider-${i}`} my={4} />
-                    <EditableMessage
-                      key={i}
-                      message={message}
-                      onEdit={(message) => {
-                        const newInputMessages = [...inputMessagesToSave];
-                        newInputMessages[i] = message;
-                        setInputMessagesToSave(newInputMessages);
-                      }}
-                      onDelete={() => {
-                        const newInputMessages = [...inputMessagesToSave];
-                        newInputMessages.splice(i, 1);
-                        setInputMessagesToSave(newInputMessages);
-                      }}
-                      ruleMatches={datasetEntry?.matchedRules}
-                    />
-                  </>
-                );
-              })}
-              <Divider my={4} />
-              <Button
-                w="full"
-                onClick={() =>
-                  setInputMessagesToSave([...inputMessagesToSave, { role: "user", content: "" }])
-                }
-                variant="outline"
-                color="gray.500"
-                _hover={{ bgColor: "orange.100" }}
-              >
-                <HStack spacing={0}>
-                  <Text>Add Message</Text>
-                  <Icon as={BsPlus} boxSize={6} />
-                </HStack>
-              </Button>
+              <VStack w="full" alignItems="flex-start">
+                <Text fontWeight="bold">Output</Text>
+                <Divider my={4} />
+                <EditableMessage
+                  message={outputMessageToSave}
+                  onEdit={(message) => setOutputMessageToSave(message)}
+                  isOutput
+                />
+              </VStack>
             </VStack>
-            <VStack w="full" alignItems="flex-start">
-              <Text fontWeight="bold">Output</Text>
-              <Divider my={4} />
-              <EditableMessage
-                message={outputMessageToSave}
-                onEdit={(message) => setOutputMessageToSave(message)}
-                isOutput
-              />
-            </VStack>
-          </VStack>
+          )}
         </DrawerBody>
         <DrawerFooter bgColor="orange.50">
           <HStack>
