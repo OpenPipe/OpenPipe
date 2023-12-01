21d04830161c68c89465230bbb855cd276d2d9e2
David Corbitt <davidlcorbitt@gmail.com>
2023-12-01 11:17:45 -0800

    Change results tab key to "results"

--- app/src/components/evals/EvalContentTabs/EvalContentTabs.tsx
+++ app/src/components/evals/EvalContentTabs/EvalContentTabs.tsx
@@ -2,7 +2,7 @@ import Results from "./Results/Results";
 import Settings from "./Settings/Settings";
 import ContentTabs from "~/components/ContentTabs";
 
-export const EVAL_RESULTS_TAB_KEY = "general";
+export const EVAL_RESULTS_TAB_KEY = "results";
 export const EVAL_SETTINGS_TAB_KEY = "settings";
 
 const tabs = [
