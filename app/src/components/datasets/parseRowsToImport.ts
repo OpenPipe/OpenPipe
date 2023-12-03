2391bef016fd9e45481bddb0132c4e851000ec34
David Corbitt <davidlcorbitt@gmail.com>
2023-12-02 19:08:59 -0800

    Allow and document OpenAI import format

--- app/src/components/datasets/parseRowsToImport.ts
+++ app/src/components/datasets/parseRowsToImport.ts
@@ -1,6 +1,9 @@
 import { validate } from "jsonschema";
 import { isObject } from "lodash-es";
-import type { ChatCompletionMessageParam } from "openai/resources/chat";
+import type {
+  ChatCompletionCreateParamsBase,
+  ChatCompletionMessageParam,
+} from "openai/resources/chat/completions";
 import { z } from "zod";
 import {
   chatCompletionInputReqPayload,
@@ -24,6 +27,16 @@ export const rowSchema = z.object({
     response_format: chatInputs.response_format,
   }),
   output: chatCompletionMessage,
+  split: z.enum(["TRAIN", "TEST"]).optional(),
+});
+
+export const openAIRowSchema = z.object({
+  messages: z.array(chatMessage),
+  function_call: functionCallInput,
+  functions: functionsInput,
+  tool_choice: toolChoiceInput,
+  tools: toolsInput,
+  response_format: chatInputs.response_format,
   split: z.enum(["TRAIN", "TEST"]).optional(),
 });
 
@@ -67,53 +80,43 @@ export const validateRowToImport = (row: unknown): ParseError | RowToImport => {
     row.output.content = null;
   }
 
-  const parsedRow = rowSchema.safeParse(row);
+  const parsedRowLegacyFormat = rowSchema.safeParse(row);
 
-  if (parsedRow.success === false) return { error: parsedRow.error.message };
-
-  const messages = parsedRow.data.input.messages;
-  //   Validate input
-  if (
-    messages.some(
-      (x) =>
-        !x.content &&
-        !("function_call" in x && x.function_call) &&
-        !("tool_calls" in x && x.tool_calls),
-    )
-  )
-    return { error: "input contains item with no content, function_call, or tool_calls" };
-  if (messages.some((x) => "function_call" in x && x.function_call && !x.function_call.arguments))
-    return { error: "input contains item with function_call but no arguments" };
-
-  //   Validate output
-  const output = parsedRow.data.output;
-  if (output) {
-    if (!output.content && !output.function_call && !output.tool_calls)
-      return { error: "output contains no content, function_call, or tool_calls" };
-    if (
-      (output.content && output.function_call) ||
-      (output.content && output.tool_calls) ||
-      (output.function_call && output.tool_calls)
-    )
-      return {
-        error: "output contains more than one of the following: content|function_call|tool_calls",
-      };
-    if (output.function_call) {
-      const _err = parseFunctionCall(output.function_call, parsedRow.data.input.functions);
-      // TODO: Add error tag to dataset entry
+  if (parsedRowLegacyFormat.success) {
+    const messages = [
+      ...parsedRowLegacyFormat.data.input.messages,
+      parsedRowLegacyFormat.data.output,
+    ];
+    for (const message of messages) {
+      const err = parseMessage(message, parsedRowLegacyFormat.data.input.functions);
+      if (err) return err;
     }
-    if (output.tool_calls) {
-      if (!Array.isArray(output.tool_calls)) return { error: "output tool_calls is not an array" };
-      for (const toolCall of output.tool_calls) {
-        if (!toolCall.function)
-          return { error: "output tool_calls contains item with no function" };
-        const _err = parseFunctionCall(toolCall.function, parsedRow.data.input.functions);
-        // TODO: Add error tag to dataset entry
-      }
-    }
+    return parsedRowLegacyFormat.data;
   }
 
-  return parsedRow.data;
+  const parsedRow = openAIRowSchema.safeParse(row);
+
+  if (parsedRow.success) {
+    for (const message of parsedRow.data.messages) {
+      const err = parseMessage(message, parsedRow.data.functions);
+      if (err) return err;
+    }
+    const { messages, split, ...rest } = parsedRow.data;
+    const output = messages.pop();
+    if (!output) return { error: "messages array is empty" };
+    if (output.role !== "assistant")
+      return { error: "the final message must be from the assistant" };
+    return {
+      input: {
+        ...rest,
+        messages,
+      },
+      split,
+      output,
+    };
+  }
+
+  return { error: parsedRow.error.message };
 };
 
 export const isParseError = (row: ParsedRow): row is ParseError => {
@@ -122,6 +125,47 @@ export const isParseError = (row: ParsedRow): row is ParseError => {
 
 export const isRowToImport = (row: ParsedRow): row is RowToImport => {
   return !isParseError(row);
+};
+
+const parseMessage = (
+  message: ChatCompletionMessageParam,
+  functions: ChatCompletionCreateParamsBase["functions"],
+) => {
+  if (
+    !message.content &&
+    (!("function_call" in message) || !message.function_call) &&
+    (!("tool_calls" in message) || !message.tool_calls)
+  )
+    return { error: "input contains item with no content, function_call, or tool_calls" };
+  if ("function_call" in message && message.function_call && !message.function_call.arguments)
+    return { error: "input contains item with function_call but no arguments" };
+
+  if (message.role === "assistant") {
+    if (!message.content && !message.function_call && !message.tool_calls)
+      return { error: "message contains no content, function_call, or tool_calls" };
+    if (
+      (message.content && message.function_call) ||
+      (message.content && message.tool_calls) ||
+      (message.function_call && message.tool_calls)
+    )
+      return {
+        error: "message contains more than one of the following: content|function_call|tool_calls",
+      };
+    if (message.function_call) {
+      const _err = parseFunctionCall(message.function_call, functions);
+      // TODO: Add error tag to dataset entry
+    }
+    if (message.tool_calls) {
+      if (!Array.isArray(message.tool_calls))
+        return { error: "message tool_calls is not an array" };
+      for (const toolCall of message.tool_calls) {
+        if (!toolCall.function)
+          return { error: "message tool_calls contains item with no function" };
+        const _err = parseFunctionCall(toolCall.function, functions);
+        // TODO: Add error tag to dataset entry
+      }
+    }
+  }
 };
 
 const parseFunctionCall = (
