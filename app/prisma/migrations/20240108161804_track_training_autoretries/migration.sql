5984704f88ab9c6ba06fbc14bb110d408133a256
David Corbitt <davidlcorbitt@gmail.com>
2024-01-08 08:31:02 -0800

    Remove all indexes from migration

--- app/prisma/migrations/20240108161804_track_training_autoretries/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE "FineTune" ADD COLUMN     "numTrainingAutoretries" INTEGER NOT NULL DEFAULT 0;
-
--- CreateIndex
-CREATE INDEX "FineTuneTrainingEntry_fineTuneId_idx" ON "FineTuneTrainingEntry"("fineTuneId");
