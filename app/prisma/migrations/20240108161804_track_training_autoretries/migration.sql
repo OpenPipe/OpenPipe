24ed61d269f6605aeb61ab30cd930bd6eadf5860
David Corbitt <davidlcorbitt@gmail.com>
2024-01-08 08:19:14 -0800

    Remove LoggedCallTag index

--- app/prisma/migrations/20240108161804_track_training_autoretries/migration.sql
+++ app/prisma/migrations/20240108161804_track_training_autoretries/migration.sql
@@ -3,6 +3,3 @@ ALTER TABLE "FineTune" ADD COLUMN     "numTrainingAutoretries" INTEGER NOT NULL 
 
 -- CreateIndex
 CREATE INDEX "FineTuneTrainingEntry_fineTuneId_idx" ON "FineTuneTrainingEntry"("fineTuneId");
-
--- CreateIndex
-CREATE INDEX "LoggedCallTag_name_projectId_idx" ON "LoggedCallTag"("name", "projectId");
