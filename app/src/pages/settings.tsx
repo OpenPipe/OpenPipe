0124c97a6878197c5456ae8ec35089d586e25c33
David Corbitt <davidlcorbitt@gmail.com>
2024-01-15 10:01:34 -0800

    Redirect to correct settings page from docs

---
+++ app/src/pages/settings.tsx
@@ -0,0 +1,38 @@
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+import { useSession } from "next-auth/react";
+
+import { useProjectList } from "~/utils/hooks";
+
+const Settings = () => {
+  const router = useRouter();
+
+  const session = useSession();
+
+  const projectList = useProjectList().data;
+
+  useEffect(() => {
+    if (session.status === "loading") return;
+
+    const redirectProject = async () => {
+      if (!projectList) return;
+      const { projects, lastViewedProjectSlug } = projectList;
+      const redirectProjectSlug = lastViewedProjectSlug || projects[0]?.slug;
+      if (!redirectProjectSlug) return;
+      await router.push({
+        pathname: "/p/[projectSlug]/settings",
+        query: { projectSlug: redirectProjectSlug },
+      });
+    };
+
+    if (session.data?.user) {
+      void redirectProject();
+    } else {
+      void router.push("/account/signin");
+    }
+  }, [session.status, session.data, router, projectList]);
+
+  return null;
+};
+
+export default Settings;
