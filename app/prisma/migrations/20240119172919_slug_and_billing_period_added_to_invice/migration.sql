69096f437b1e4e3e9d14bccbb36203482eb2e415
mac <kovsbo@gmail.com>
2024-01-23 14:18:00 -0800

    migrations squashed

--- app/prisma/migrations/20240119172919_slug_and_billing_period_added_to_invice/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - Added the required column `slug` to the `Invoice` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE "Invoice" ADD COLUMN     "billingPeriod" TEXT,
-ADD COLUMN     "slug" TEXT NOT NULL;
