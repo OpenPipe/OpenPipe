77d18bdb349a3daebf37465e8d86d97c56fd52d4
David Corbitt <davidlcorbitt@gmail.com>
2024-02-02 16:59:55 -0800

    Persist sidebarExpanded

--- app/src/components/nav/AppShell.tsx
+++ app/src/components/nav/AppShell.tsx
@@ -11,6 +11,7 @@ import {
   Tooltip,
   type BoxProps,
   IconButton,
+  useBreakpointValue,
 } from "@chakra-ui/react";
 import Head from "next/head";
 import Link from "next/link";
@@ -38,6 +39,12 @@ const NavSidebar = () => {
   const sidebarExpanded = useAppStore((state) => state.sidebarExpanded);
   const setSidebarExpanded = useAppStore((state) => state.setSidebarExpanded);
 
+  // useBreakpointValue is more reliable when initializing than chakra's built-in breakpoint values
+  const width = useBreakpointValue({
+    base: "56px",
+    md: sidebarExpanded ? "240px" : "56px",
+  });
+
   return (
     <VStack
       align="stretch"
@@ -45,7 +52,7 @@ const NavSidebar = () => {
       px={2}
       pb={0}
       height="100%"
-      w={{ base: "56px", md: sidebarExpanded ? "240px" : "56px" }}
+      w={width}
       transition="width 0.2s ease-in-out"
       overflow="hidden"
       borderRightWidth={1}
