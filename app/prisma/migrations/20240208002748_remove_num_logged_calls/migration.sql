3dcfd27197f225641052173817372c8f632651a7
David Corbitt <davidlcorbitt@gmail.com>
2024-02-07 16:29:53 -0800

    Stop caching numLoggedCalls

---
+++ app/prisma/migrations/20240208002748_remove_num_logged_calls/migration.sql
@@ -0,0 +1,11 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `numLoggedCalls` on the `Project` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "Project" DROP COLUMN "numLoggedCalls";
+
+DROP TRIGGER IF EXISTS trigger_increment_num_logged_calls ON "LoggedCall";
+DROP FUNCTION IF EXISTS increment_num_logged_calls();
