6b48b477a0edbff589853866a33c8897b9396a29
Bohdan Kovalevskyi <kovsbo@gmail.com>
2024-02-12 17:57:44 -0800

    test email script updated

--- app/src/server/scripts/testSendInvoiceEmailToExistingClients.ts
+++ app/src/server/scripts/testSendInvoiceEmailToExistingClients.ts
@@ -2,14 +2,13 @@ import { prisma } from "../db";
 import { sendInvoiceNotificationWithoutRequiredPayment } from "../emails/sendInvoiceNotificationWithoutRequiredPayment";
 import { sendToAdmins } from "../emails/sendToAdmins";
 
-const creditAdjId = process.argv[2];
+const email = process.argv[2];
 
 console.log("Doing: sending emiails");
 
 //Neagative credits mean that the servcie was used
 const creditAdjustments = await prisma.creditAdjustment.findMany({
   where: {
-    id: creditAdjId,
     amount: {
       lt: 0,
     },
@@ -27,20 +26,19 @@ for (const creditAdjustment of creditAdjustments) {
   // Searching for pending invoices, and invoices with usage, but covered by credits
   // amount > -100 means that the credits covered all the usage
   if (
+    email &&
     project.billable &&
     invoice &&
     (invoice.status === "PENDING" || Number(creditAdjustment.amount) > -100)
   ) {
-    await sendToAdmins(invoice.projectId, (email: string) =>
-      sendInvoiceNotificationWithoutRequiredPayment(
-        invoice.id,
-        Number(invoice.amount),
-        invoice.description,
-        invoice.billingPeriod || "",
-        project.name,
-        project.slug,
-        email,
-      ),
+    await sendInvoiceNotificationWithoutRequiredPayment(
+      invoice.id,
+      Number(invoice.amount),
+      invoice.description,
+      invoice.billingPeriod || "",
+      project.name,
+      project.slug,
+      email,
     );
   }
 }
