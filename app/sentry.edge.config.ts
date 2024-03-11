5ddc023617d54233d27a8be9fb4dee98ce5c00fa
arcticfly <davidlcorbitt@gmail.com>
2024-03-11 13:36:28 -0700

    Decrease edge sample rate

--- app/sentry.edge.config.ts
+++ app/sentry.edge.config.ts
@@ -11,7 +11,7 @@ if (env.NEXT_PUBLIC_SENTRY_DSN) {
     dsn: env.NEXT_PUBLIC_SENTRY_DSN,
 
     // Adjust this value in production, or use tracesSampler for greater control
-    tracesSampleRate: 1,
+    tracesSampleRate: 0.1,
 
     // Setting this option to true will print useful information to the console while you're setting up Sentry.
     debug: false,
