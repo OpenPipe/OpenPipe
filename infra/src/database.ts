47a4339a28422386c74141ccac86b619ec8ddc45
Bohdan Kovalevskyi <kovsdan@gmail.com>
2024-03-13 09:28:46 -0700

    Database VPN access only (#683)
    * db firewall added
    
    * more IPs added
    
    ---------
    
    Co-authored-by: Bohdan Kovalevskyi <kovsbo@gmail.com>

--- infra/src/database.ts
+++ infra/src/database.ts
@@ -2,11 +2,21 @@ import * as aws from "@pulumi/aws";
 import * as pulumi from "@pulumi/pulumi";
 import * as random from "@pulumi/random";
 import { nm } from "./helpers";
-import { vpc } from "./vpc";
+import { vpc, vpcCidrBlocks } from "./vpc";
+
+const cfg = new pulumi.Config();
+const allowedListArray = cfg.require("ALLOWED_IP_LIST").split(",");
 
 const dbSecurityGroup = new aws.ec2.SecurityGroup(nm("app-db"), {
   vpcId: vpc.vpcId,
-  ingress: [{ protocol: "tcp", fromPort: 5432, toPort: 5432, cidrBlocks: ["0.0.0.0/0"] }],
+  ingress: [
+    {
+      protocol: "tcp",
+      fromPort: 5432,
+      toPort: 5432,
+      cidrBlocks: [vpcCidrBlocks, ...allowedListArray],
+    },
+  ],
 });
 
 const dbSubnetGroup = new aws.rds.SubnetGroup(nm("app-db"), {
@@ -103,7 +113,7 @@ const dmsAssumeRole = aws.iam.getPolicyDocument({
 });
 const dms_access_for_endpoint = new aws.iam.Role(nm("dms-access-for-endpoint"), {
   assumeRolePolicy: dmsAssumeRole.then((dmsAssumeRole) => dmsAssumeRole.json),
-  name: nm("dms-access-for-endpoint"),
+  name: isProd ? "dms-access-for-endpoint" : nm("dms-access-for-endpoint"),
 });
 
 const dms_access_for_endpoint_AmazonDMSRedshiftS3Role = new aws.iam.RolePolicyAttachment(
@@ -116,7 +126,7 @@ const dms_access_for_endpoint_AmazonDMSRedshiftS3Role = new aws.iam.RolePolicyAt
 
 const dms_cloudwatch_logs_role = new aws.iam.Role(nm("dms-cloudwatch-logs-role"), {
   assumeRolePolicy: dmsAssumeRole.then((dmsAssumeRole) => dmsAssumeRole.json),
-  name: nm("dms-cloudwatch-logs-role"),
+  name: isProd ? "dms-cloudwatch-logs-role" : nm("dms-cloudwatch-logs-role"),
 });
 
 const dms_cloudwatch_logs_role_AmazonDMSCloudWatchLogsRole = new aws.iam.RolePolicyAttachment(
--- infra/src/vpc.ts
+++ infra/src/vpc.ts
@@ -1,7 +1,9 @@
 import * as awsx from "@pulumi/awsx";
 import { nm } from "./helpers";
 
+export const vpcCidrBlocks = "10.0.0.0/16";
+
 export const vpc = new awsx.ec2.Vpc(nm("main"), {
   enableDnsHostnames: true,
-  cidrBlock: "10.0.0.0/16",
+  cidrBlock: vpcCidrBlocks,
 });
