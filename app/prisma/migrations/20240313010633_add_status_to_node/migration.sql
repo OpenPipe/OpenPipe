e77e728b48babeed28cff262210d81f4e1c795dc
arcticfly <davidlcorbitt@gmail.com>
2024-03-21 23:23:12 -0700

    Move index creations to the bottom of the migration

--- app/prisma/migrations/20240313010633_add_status_to_node/migration.sql
+++ app/prisma/migrations/20240313010633_add_status_to_node/migration.sql
@@ -1,23 +1,12 @@
 -- CreateEnum
 CREATE TYPE "NodeStatus" AS ENUM ('IDLE', 'QUEUED', 'PROCESSING');
 
--- DropIndex
-DROP INDEX "LoggedCall_projectId_model_updatedAt_idx";
-
 -- AlterTable
 ALTER TABLE "Node" ADD COLUMN     "status" "NodeStatus" NOT NULL DEFAULT 'IDLE';
-
--- CreateIndex
-CREATE INDEX "LoggedCall_projectId_model_updatedAt_id_idx" ON "LoggedCall"("projectId", "model", "updatedAt", "id");
 
 -- AlterTable
 ALTER TABLE "NodeEntry" ALTER COLUMN "originalOutputHash" DROP NOT NULL;
 
--- DropIndex
-DROP INDEX "LoggedCallTag_loggedCallId_idx";
-
--- CreateIndex
-CREATE INDEX "LoggedCallTag_loggedCallId_name_value_idx" ON "LoggedCallTag"("loggedCallId", "name", "value");
 
 BEGIN;
 -- AlterEnum
@@ -26,3 +15,14 @@ COMMIT;
 
 -- AlterTable
 ALTER TABLE "NodeEntry" ALTER COLUMN "status" SET DEFAULT 'QUEUED';
+
+
+-- DropIndex
+DROP INDEX "LoggedCall_projectId_model_updatedAt_idx";
+-- DropIndex
+DROP INDEX "LoggedCallTag_loggedCallId_idx";
+
+-- CreateIndex
+CREATE INDEX "LoggedCallTag_loggedCallId_name_value_idx" ON "LoggedCallTag"("loggedCallId", "name", "value");
+-- CreateIndex
+CREATE INDEX "LoggedCall_projectId_model_updatedAt_id_idx" ON "LoggedCall"("projectId", "model", "updatedAt", "id");
